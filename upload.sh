263ef96348b967d4d7029ac22e5de37cd762c373
neozhaoliang <mathzhaoliang@gmail.com>
2025-10-10 00:29:45 +0800

    save

--- upload.sh
+++ upload.sh
@@ -3,13 +3,4 @@ git commit -m "save"
 git push
 
 hexo g
-cp CNAME ./public
-zip -r source.zip ./source
-zip -r layout.zip ./themes/raytaylorism/layout/
-zip -r drafts.zip ./_drafts/
-mv source.zip ./public/
-mv layout.zip ./public/
-mv drafts.zip ./public/
-cp ref.bib ./public
-cp metadata.yml ./public
 hexo d
